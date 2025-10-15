434f5fd38b00b16595843af0700e8fd1d36cbb44
Anthony Shaw <anthony.p.shaw@gmail.com>
2025-10-15 04:11:08 +0000

    fix app launch

--- app/api/app.py
+++ app/api/app.py
@@ -415,9 +415,8 @@ if __name__ == "__main__":
     import uvicorn
     
     uvicorn.run(
-        "app.api.app:app",
+        app,
         host="0.0.0.0",
         port=8091,
-        reload=True,
         log_level="info"
     )
