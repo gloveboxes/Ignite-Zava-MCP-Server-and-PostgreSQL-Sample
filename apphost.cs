d3ac8a369e7b31e2df1f4a7832cc37cbc9691ba8
Anthony Shaw <anthony.p.shaw@gmail.com>
2025-10-18 00:17:11 +0000

    Update apphost

--- apphost.cs
+++ apphost.cs
@@ -1,10 +1,12 @@
 #:sdk Aspire.AppHost.Sdk@13.0.0-preview.1.25517.6
 #:package Aspire.Hosting.NodeJs@13.0.0-preview.1.25517.6
 #:package Aspire.Hosting.Python@13.0.0-preview.1.25517.6
+#:package CommunityToolkit.Aspire.Hosting.NodeJS.Extensions@9.8.0
 
 var builder = DistributedApplication.CreateBuilder(args);
+#pragma warning disable ASPIREHOSTINGPYTHON001
 
-api = builder.AddPythonModule("api_service", "./app/agents/src", "uvicorn").
+var apiService = builder.AddPythonModule("api", "./app/api/", "uvicorn")
     .WithArgs("github_shop_api.app:app", "--reload")
     .WithUvEnvironment()
     .WithHttpEndpoint(env: "UVICORN_PORT")
